d03f1bba2f24137d5dd567dfb87407deb8a313e7
mackliu <macklun@gmail.com>
2020-05-14 16:25:36 +0800

    all方法中加入第三個字串變數

--- all.php
+++ all.php
@@ -1,7 +1,6 @@
 <?php
 include "base.php";
 
-
 /**
  * "select * from $table ";
  * "select * from $table where ???='xxx'";
@@ -29,16 +28,17 @@ function all($table,...$arg){
         foreach($arg[0] as $key => $value){
             $tmp[]=sprintf("`%s`='%s'",$key,$value);
         }
-       
         $sql=$sql." where " . implode(" && ",$tmp);
+    }
+
+    if(isset($arg[1])){
+        $sql=$sql . $arg[1];
     }
 
     //echo $sql;
 
     return $pdo->query($sql)->fetchAll();
 }
-
-
 
 $rows=all('invoice');
 foreach($rows as $row){
@@ -50,7 +50,27 @@ foreach($rows as $row){
     echo "<hr>";
 }
 echo "<p>有帶參數</p>";
-$rows=all('invoice',["year"=>"2020","period"=>"1","code"=>"AA"]);
+$rows=all('invoice',["year"=>"2020","period"=>"1"]);
+foreach($rows as $row){
+    echo $row['id'] ."-";
+    echo $row['code'] ."-";
+    echo $row['number'] ."-";
+    echo $row['period'] ."-";
+    echo $row['expend'] ;
+    echo "<hr>";
+}
+echo "<p>有帶三個參數</p>";
+$rows=all('invoice',["year"=>"2020","period"=>"1"]," order by id desc");
+foreach($rows as $row){
+    echo $row['id'] ."-";
+    echo $row['code'] ."-";
+    echo $row['number'] ."-";
+    echo $row['period'] ."-";
+    echo $row['expend'] ;
+    echo "<hr>";
+}
+echo "<p>不帶條件參數</p>";
+$rows=all('invoice',""," order by id desc");
 foreach($rows as $row){
     echo $row['id'] ."-";
     echo $row['code'] ."-";
