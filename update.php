1f34185a65e1ef180a2e87eb65cd5fae6e657256
mackliu <macklun@gmail.com>
2020-05-15 15:33:09 +0800

    更新特定資料函式

---
+++ update.php
@@ -0,0 +1,48 @@
+<?php
+include_once "base.php";
+
+/***
+ * 更新資料
+ * 
+ * update table set xx='aa',yy='bb,zz='cc'
+ * update table set xx='aa',yy='bb,zz='cc' where xx='aa' && yy='bb'
+ * update table set xx='aa',yy='bb,zz='cc' where id='?'
+ * 
+ */
+$table='invoice';
+
+$row=find($table,1);
+echo "<pre>";
+print_r($row);
+echo "</pre>";
+
+$row['code']="ZZ";
+
+update($table,$row);
+
+
+function update($table,$arg){
+    global $pdo;
+    
+    foreach($arg as $key => $value){
+        $tmp[]=sprintf("`%s`='%s'",$key,$value);
+    }
+
+    $sql="update $table set ".implode(',',$tmp)." where `id`='".$arg['id']."'";
+    echo $sql;
+    //return $pdo->exec($sql);
+}
+
+
+
+function find($table,$id){
+    global $pdo;
+    $sql="select * from $table where id='$id'";
+    $row=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    if(empty($row)){
+        return "無符合資料的內容";
+    }
+    return $row;
+}
+
+?>
